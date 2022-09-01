83933d5b39a58cbd0867f57829436d3f6190ec78
Andy Ross <andyross@google.com>
2022-09-01 08:36:37 -0700

    squash posix

--- zephyr/posix.c
+++ zephyr/posix.c
@@ -1,4 +1,5 @@
 #include <sof/ipc/driver.h>
+#include <sof/schedule/task.h>
 #include <sof/platform.h>
 
 uint8_t posix_hostbox[MAILBOX_HOSTBOX_SIZE];
@@ -19,6 +20,18 @@ __asm__(".globl _module_init_start\n"
 	".globl _module_init_end\n"
 	"_module_init_end:\n");
 
+/* Ditto for symbols in .trace_ctx */
+__asm__(".globl _trace_ctx_start\n"
+	"_trace_ctx_start:\n"
+	".globl _trace_ctx_end\n"
+	"_trace_ctx_end:\n");
+
+
+int platform_ipc_init(struct ipc *ipc)
+{
+	return 0;
+}
+
 int ipc_platform_send_msg(const struct ipc_msg *msg)
 {
 	return 0;
@@ -28,8 +41,51 @@ void ipc_platform_complete_cmd(struct ipc *ipc)
 {
 }
 
+enum task_state ipc_platform_do_cmd(struct ipc *ipc)
+{
+	return SOF_TASK_STATE_COMPLETED;
+}
+
+struct ipc_data_host_buffer *ipc_platform_get_host_buffer(struct ipc *ipc)
+{
+	return NULL;
+}
+
+void mtrace_event(const char *data, uint32_t length)
+{
+}
+
+int dmac_init(struct sof *sof)
+{
+        return 0;
+}
+
+int dai_init(struct sof *sof)
+{
+}
+
+int platform_context_save(struct sof *sof)
+{
+	return 0;
+}
+
+void platform_clock_init(struct sof *sof)
+{
+	// FIXME: seems like this has to initialize sof->clocks */
+}
+
 int platform_init(struct sof *sof)
 {
+	/* All this seems to be generic boilerplate duplicated in all
+	 * platform_init() mathods?
+	 */
+        platform_clock_init(sof);
+        scheduler_init_edf();
+        sa_init(sof, CONFIG_SYSTICK_PERIOD);
+        dmac_init(sof);
+        ipc_init(sof);
+        dai_init(sof);
+
 	return 0;
 }
 
