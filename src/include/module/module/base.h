7f9fbd3840c967f41314ca6de3d82b7158419daa
Andy Ross <andyross@google.com>
2024-01-02 11:29:05 -0800

    module_adapter_ipc4: Save and pre-parse base_cfg_ext data
    The kernel-provided "base_cfg_ext" data wasn't being handled correctly
    by the module adapter layer.  These structs (packed wire formats) only
    ever lived in the IPC memory.  The module would set them on an
    "init_data" before calling into driver init, and then clear that
    poitner afterwards.  That's a critical problem, because the values in
    that struct need to be used at pipeline setup time to configure buffer
    formats!
    
    Save the data correctly.  Also pre-parse it so users don't need to do
    byte math on raw buffers and can just use "in/output_pins[]" arrays on
    the module_config struct.
    
    Signed-off-by: Andy Ross <andyross@google.com>

--- src/include/module/module/base.h
+++ src/include/module/module/base.h
@@ -30,7 +30,12 @@ struct module_config {
 	const void *init_data; /**< Initial IPC configuration. */
 #if CONFIG_IPC_MAJOR_4
 	struct ipc4_base_module_cfg base_cfg;
-	struct ipc4_base_module_cfg_ext *basecfg_ext;
+	uint8_t nb_input_pins;
+	uint8_t nb_output_pins;
+	struct ipc4_input_pin_format *input_pins;
+	struct ipc4_output_pin_format *output_pins;
+	struct ipc4_base_module_cfg_ext *basecfg_ext; /* legacy API */
+
 #endif
 };
 
