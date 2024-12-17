523058f8924102e583d840414dceb36f052e7a0e
Andy Ross <andyross@google.com>
2024-12-16 18:13:54 -0800

    BIG SQUASH

--- scripts/xtensa-build-zephyr.py
+++ scripts/xtensa-build-zephyr.py
@@ -102,6 +102,11 @@ extra_platform_configs = {
 		"ace30_LX7HiFi4_PIF",
 		ipc4 = True
 	),
+	"mt8196" : PlatformConfig(
+		"mtk", "mt8196/mt8196/adsp",
+		f"RJ-2024.3{xtensa_tools_version_postfix}",
+		"HiFi5_MPU_lock_2023_11",
+	),
 }
 
 # These can all be built out of the box. --all builds all these.
@@ -1153,7 +1158,7 @@ RI_INFO_UNSUPPORTED = []
 
 RI_INFO_UNSUPPORTED += ['imx8', 'imx8x', 'imx8m', 'imx8ulp']
 RI_INFO_UNSUPPORTED += ['rn']
-RI_INFO_UNSUPPORTED += ['mt8186', 'mt8195']
+RI_INFO_UNSUPPORTED += ['mt8186', 'mt8188', 'mt8195', 'mt8196']
 
 # For temporary workarounds. Unlike _UNSUPPORTED above, the platforms below will print a warning.
 RI_INFO_FIXME = [ ]
--- zephyr/lib/alloc.c
+++ zephyr/lib/alloc.c
@@ -89,6 +89,15 @@ char __aligned(8) heapmem[HEAPMEM_SIZE];
  */
 static uint8_t __aligned(PLATFORM_DCACHE_ALIGN) heapmem[HEAPMEM_SIZE];
 
+#elif defined(CONFIG_SOC_FAMILY_MTK)
+
+extern char _mtk_adsp_sram_end;
+#define SRAM_START DT_REG_ADDR(DT_NODELABEL(sram0))
+#define SRAM_SIZE  DT_REG_SIZE(DT_NODELABEL(sram0))
+#define SRAM_END   (SRAM_START + SRAM_SIZE)
+#define heapmem ((uint8_t *)ALIGN_UP((uintptr_t)&_mtk_adsp_sram_end, PLATFORM_DCACHE_ALIGN))
+#define HEAPMEM_SIZE ((uint8_t *)SRAM_END - heapmem)
+
 #else
 
 extern char _end[], _heap_sentry[];
